8a8a5a59c50ba1498c25eb9845582f08749bbd09
Christian <christian@girasolo.com>
2021-06-15 17:45:50 -0500

    Added docs

--- src/helpers/getGifts.js
+++ src/helpers/getGifts.js
@@ -1,5 +1,5 @@
 export const getGifts = async (category) => {
-  const url = `http://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=hDgkUw0FBksnhXDEG9PB3c0bbMnKuY8Z`;
+  const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=hDgkUw0FBksnhXDEG9PB3c0bbMnKuY8Z`;
   const resp = await fetch(url);
   const { data } = await resp.json();
 
